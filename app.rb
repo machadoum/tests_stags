34d4772c5c285adf4a34c10759226c1325f01b23
Cristian Oliveira <contato@cristianoliveira.com.br>
2016-08-10 23:31:04 -0300

    uglify design

--- app.rb
+++ app.rb
@@ -2,9 +2,6 @@ require_relative 'lib/path_builder'
 require_relative 'lib/random_path_builder'
 require_relative 'lib/file_mover'
 
-home = "."
 file = "myfile.txt"
-origin_builder = PathBuilder.new(home)
-dest_builder = RandomPathBuilder.new(home)
-mover = FileMover.new origin_builder, dest_builder
+mover = FileMover.new
 mover.move file
